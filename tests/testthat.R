0c9edc57d181f97cb8fedda6978d52cb0f0e2887
Cole Arendt <cole@digitalrocket.biz>
2025-07-02 05:04:25 -0400

    add tests

---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(xlsxjars)
+
+test_check("xlsxjars")
---
+++ tests/testthat/test-package.R
@@ -0,0 +1,19 @@
+test_that("Package has correct structure", {
+  expect_true(file.exists(system.file("DESCRIPTION", package = "xlsxjars")),
+              "DESCRIPTION file should exist")
+  
+  expect_true(file.exists(system.file("COPYRIGHTS", package = "xlsxjars")),
+              "COPYRIGHTS file should exist")
+  
+  expect_true(dir.exists(system.file("java", package = "xlsxjars")),
+              "java directory should exist")
+})
+
+test_that("Package metadata is correct", {
+  desc <- packageDescription("xlsxjars")
+  
+  expect_equal(desc$Package, "xlsxjars")
+  expect_match(desc$Title, "POI jars")
+  expect_equal(desc$License, "GPL-3")
+  expect_match(desc$Description, "xlsxjars package")
+})
